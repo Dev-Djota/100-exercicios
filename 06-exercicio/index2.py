7ccb7599b1391f047fa14533420886708ec5f934
Dev-Djota <danillog366@gmail.com>
2025-05-20 22:33:51 -0300

    exercicio 6

---
+++ 06-exercicio/index2.py
@@ -0,0 +1,4 @@
+medida = float(input('Deseja quantos metros:'))
+
+print(f'Sua medida em metros é {medida}')
+print(f'Sua medida em centimetros é {medida * 100} \nA sua medida em milimetros é {medida * 1000}')
